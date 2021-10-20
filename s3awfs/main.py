a222317ea27417e9933e3d6ee7f31a69d28781f1
njessurun <ntjessu@gmail.com>
2021-10-20 15:06:52 -0400

    Lazy addition of `help` kwarg

--- s3awfs/main.py
+++ s3awfs/main.py
@@ -1,15 +1,17 @@
 from __future__ import annotations
 
 from pathlib import Path
+import sys
 
 # Some systems need qt initialized before cv gets imported
 import pyqtgraph as pg
+pg.mkQApp()
 
 from generic import MainWorkflow
+from workflows import allWorkflows
 from workflows.constants import DEFAULT_RESIZE_OPTS, SMD_INIT_OPTS
 from workflows.utils import argparseHelpAction
 
-pg.mkQApp()
 
 from utilitys import fns
 
@@ -22,13 +24,15 @@ def main_rgbFeatures512(**kwargs):
     folder = kwargs['folder'] = Path(kwargs.pop('folder', None) or Path.home()/'Desktop/rgb_features_512')
     kwargs['labelMapFile'] = kwargs.get('labelMapFile') or folder/'aliased_labels.csv'
     init, run = MainWorkflow.splitInitAndRunKwargs(kwargs)
-    mwf = MainWorkflow(folder, **init)
+    mwf = MainWorkflow(**init)
     return mwf.run(**run)
 
 def main_cli():
     parser = fns.makeCli(main_rgbFeatures512, parserKwargs=dict(add_help=False))
-    parser.register('action', 'help', argparseHelpAction(MainWorkflow('garbageasdf', createDirs=False)))
-    parser.add_argument('--help', action='help')
+    # Expensive, avoid unless requested
+    if '--help' in sys.argv:
+        parser.register('action', 'help', argparseHelpAction(MainWorkflow('garbageasdf', allWorkflows(), createDirs=False)))
+        parser.add_argument('--help', action='help')
     kwargs = vars(parser.parse_args())
     if 'config' in kwargs:
         MainWorkflow.runFromConfig(**kwargs)
