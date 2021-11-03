f697a1b8709d11c7dedd5e4fd5441f1ad18274e0
njessurun <ntjessu@gmail.com>
2021-11-03 12:12:29 -0400

    Adds viewer widget for seeing one prediction across epochs

--- s3awfs/workflows/models/common.py
+++ s3awfs/workflows/models/common.py
@@ -4,17 +4,19 @@ import os
 from pathlib import Path
 
 import cv2 as cv
+import imageio
 import numpy as np
 import pandas as pd
+import pyqtgraph as pg
 import tensorflow as tf
-from s3a import generalutils as gutils
+from s3awfs.workflows.constants import RNG
 from skimage.measure import regionprops, label
 from tensorboard.backend.event_processing import event_accumulator
 from tensorflow.keras.utils import Sequence
 from tensorflow.python.keras.utils.np_utils import to_categorical
 
-from s3awfs.workflows.constants import RNG
-
+from s3a import generalutils as gutils
+from utilitys import fns, widgets, RunOpts
 
 class SequenceDataGenerator(Sequence):
     """
@@ -118,8 +120,6 @@ class SquareMaskSequenceDataGenerator(SequenceDataGenerator):
             bbox = region.bbox
             mask[bbox[0]:bbox[2], bbox[1]:bbox[3]] = 1
         return to_categorical(mask, num_classes=self.numOutputClasses, dtype=np.uint8)
-
-oldgen = SequenceDataGenerator
 
 class DataGenIterator:
     def __init__(
@@ -229,3 +229,84 @@ def export_training_data(graph_path, training_name):
         )
         values_df.to_csv(csv_path)
     return values_df
+
+class EpochEvolutionViewer(widgets.ImageViewer):
+    def __init__(self, predictionsDir=None, **kwargs):
+        super().__init__(**kwargs)
+        self.predictionsDir = None
+        self._epochDirs = []
+
+        self.toolsEditor.registerFunc(self.saveEvolutionGif)
+        self.changeDataProc = self.toolsEditor.registerFunc(self.changeImage, runOpts=RunOpts.ON_CHANGED)
+
+        predProc = self.toolsEditor.registerFunc(self.changePredictionsDir, runOpts=RunOpts.ON_CHANGED)
+        if predictionsDir is not None:
+            predProc(predictionsDir=predictionsDir)
+
+    def changeImage(self,
+                   selectedImage='',
+                   selectedEpoch='',
+                   # groundTruthMasks: FilePath=None,
+                   ):
+        """
+        :param selectedImage:
+        type: list
+        :param selectedEpoch:
+        type: list
+        """
+        selectedEpoch = Path(selectedEpoch)
+        image = selectedEpoch/selectedImage
+        if not image.is_file():
+            return
+        self.setImage(image)
+
+    def changePredictionsDir(self, predictionsDir: Path=''):
+        """
+        :param predictionsDir:
+        type: filepicker
+        asFolder: True
+        """
+        self.predictionsDir = Path(predictionsDir)
+        p = self.changeDataProc.input.params
+        epochDirs = fns.naturalSorted([d for d in self.predictionsDir.iterdir() if d.is_dir()])
+        p['selectedEpoch'].setLimits({d.name: d for d in epochDirs})
+        uniqueImgs = {im.name for e in epochDirs for im in e.glob('*.*')}
+        if any(epochDirs):
+            p['selectedImage'].setLimits(fns.naturalSorted(uniqueImgs))
+        else:
+            p['selectedImage'].setLimits([])
+
+    def saveEvolutionGif(self, filename=None, fps=2, epochAsText=True):
+        """
+        :param filename:
+        type: filepicker
+        existing: False
+        value: 'predictions.gif'
+        fileFilter: Gif Files (*.gif);;
+        """
+        p = self.changeDataProc.input.params
+        ims = []
+        selected = p['selectedImage'].value()
+        drawitem = widgets.MaskCompositor()
+        for epoch in p['selectedEpoch'].opts['limits'].values():
+            imageFile = epoch/selected
+            im = gutils.cvImread_rgb(imageFile)
+            if im is None:
+                continue
+            if epochAsText:
+                drawitem.clearOverlays()
+                drawitem.setImage(im)
+                drawitem.addMask(np.zeros(im.shape[:2], 'uint8'), f'{imageFile.parent.name}/{imageFile.name}')
+                saved = drawitem.save()
+                savedAsImg = saved.toImage()
+                im = cv.cvtColor(pg.imageToArray(savedAsImg, transpose=False), cv.COLOR_BGR2RGB)
+            ims.append(im)
+        if filename is not None:
+            imageio.mimsave(filename, ims, fps=fps)
+        return ims
+
+    @classmethod
+    def main(cls, predictionsDir=None):
+        pg.mkQApp()
+        eev = cls(predictionsDir=predictionsDir)
+        eev.show_exec()
