402a103d98f5fed9c9c7ee668235b6eac02085ed
Nathan Jessurun <ntjessu@gmail.com>
2022-05-12 23:09:57 -0400

    Better default map for 2-class visualizations

--- s3awfs/constants.py
+++ s3awfs/constants.py
@@ -6,7 +6,7 @@ DEFAULT_RESIZE_OPTS = dict(shape=(512, 512), rotationDeg=PRJ_ENUMS.ROT_OPTIMAL)
 # Determines whether multiprocessing uses multiple cores or is disabled
 DEBUG = True
 
-DEFAULT_RGB_CMAP = "PAL-relaxed_bright"
+DEFAULT_RGB_CMAP = "plasma"
 
 DUMMY_FGND_VALUE = "FGND"
 
